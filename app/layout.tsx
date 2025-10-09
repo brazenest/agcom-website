1143e7a348aef2ce1222d6acc4d36f27a1c12b67
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2025-10-09 19:32:44 +0000

    Use Noto Sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Noto_Sans } from "next/font/google";
 import "./globals.css";
 import SiteHeader from "./lib/components/site-header";
 import SiteFooter from "./lib/components/site-footer";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const notoSans = Noto_Sans({
+  variable: '--font-noto-sans',
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+})
 
 export const metadata: Metadata = {
   title: "Alden Gillespy - Software Engineer, Video Editor, Photographer, Videographer",
@@ -27,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${notoSans.variable} antialiased`}
       >
         <SiteHeader />
         {children}
