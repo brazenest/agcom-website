e27289efd9ef36bb9f4759178c5bc56ebf5ad75b
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2025-11-14 07:56:00 -0700

    Perform housecleaning

--- old/functions/get-datetime-value-from-date.ts
+++
@@ -1,2 +0,0 @@
-export const getDatetimeValueFromDate = (date: Date): string => date.toISOString().slice(0, 19).replace('T', ' ');
-export const getDateObjectFromDatetimeValue = (datetimeValue: string): Date => new Date(datetimeValue);
