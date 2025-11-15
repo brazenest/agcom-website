083285f058e61c4a8afd36e2bee9f9ab2b137af0
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2025-11-15 16:13:40 -0700

    Log the info in process.env

--- functions/queryApi.ts
+++ functions/queryApi.ts
@@ -8,6 +8,7 @@ import { createApiUrl } from "./createApiUrl";
 
 export const queryApi = async ({ endpoint, params = [], method = 'GET', data = {} }: queryApiParamsT) => {
 
+    console.log('process.env ====', process.env)
     // Construct URL with query parameters
     const requestUrl = createApiUrl({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL!, endpoint, params })
 
