7ffd029c267033c1679f5c9cd62cda1a213afabc
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-01-24 19:12:34 -0700

    Implement error handling in deploy script
    Added error handling to the deployment script.

--- scripts/deploy_production_script.sh
+++ scripts/deploy_production_script.sh
@@ -1,5 +1,11 @@
+handle_error(msg) {
+  echo "A fatal error occurred: $msg"
+  exit 1
+}
+
 cd ~/webapps/agcom-website/deployments/production
 git pull origin main
+trap 'handle_error "Failed to pull from origin"' ERR
 npm ci
 npm run build
 npm run test
