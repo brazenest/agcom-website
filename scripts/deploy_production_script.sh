58802440d9361788e588afbd9b93e80ded7dc86a
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-01-26 13:27:12 -0700

    Modify deploy_production_script.sh

--- scripts/deploy_production_script.sh
+++ scripts/deploy_production_script.sh
@@ -1,12 +1,16 @@
 handle_error() {
-  echo "A fatal error occurred: $1"
+  echo "$1. Terminating script..."
   exit 1
 }
 
 cd ~/webapps/agcom-website/deployments/production
-git pull origin main
 trap 'handle_error "Failed to pull from origin"' ERR
+git pull origin main
+trap 'handle_error "Failed to install dependencies"' ERR
 yarn install --frozen-lockfile
+trap 'handle_error "Failed to build project"' ERR
 yarn build
+trap 'handle_error "Failed to run tests"' ERR
 yarn test
+trap 'handle_error "Failed to restart production server"' ERR
 pm2 restart agcom-website-production
