05b91c353a139f3622c7ae86600c811f0232ab28
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-01-26 13:50:01 -0700

    Make more changes to deploy_production_script.sh

--- scripts/deploy_production_script.sh
+++ scripts/deploy_production_script.sh
@@ -4,13 +4,32 @@ handle_error() {
 }
 
 cd ~/webapps/agcom-website/deployments/production
+
+# Clone repository if that hasn't been done yet.
+trap 'handle_error "Failed to clone repository"' ERR
+if ! [ -f "package.json" ]; then
+  rm -rf *
+  git clone github:brazenest/agcom-website.git
+  mv agcom-website/* .
+  rmdir agcom-website
+fi
+
 trap 'handle_error "Failed to pull from origin"' ERR
 git pull origin main
+
 trap 'handle_error "Failed to install dependencies"' ERR
 yarn install
+
 trap 'handle_error "Failed to build project"' ERR
 yarn build
+
 trap 'handle_error "Failed to run tests"' ERR
 yarn test
+
+trap 'handle_error "Failed to restart nginx"' ERR
+sudo nginx -s reload
+
 trap 'handle_error "Failed to restart production server"' ERR
 pm2 restart agcom-website-production
+
+echo "Successfully deployed production version of agcom-website."
