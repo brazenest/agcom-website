f60c49e9522276748e2b0d92205f21a9f093636b
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-02-01 09:17:09 -0700

    Make final chages before debug/release

--- components/newton/layout/EmphasisSection.tsx
+++ components/newton/layout/EmphasisSection.tsx
@@ -9,7 +9,7 @@ export const EmphasisSection = ({
 	actions,
 }: EmphasisSectionProps) => {
 	return (
-		<section id={id} className="emphasis-section py-12 lg:py-24 px-6 bg-blue-100 dark:bg-blue-900 border border-blue-200 dark:border-blue-800 rounded-2xl text-center max-w-4xl lg:max-w-5xl mx-auto">
+		<section id={id} className="emphasis-section py-12 lg:py-24 md:px-6 bg-blue-100 dark:bg-blue-900 border border-blue-200 dark:border-blue-800 text-center max-w-4xl lg:max-w-5xl mx-auto">
 			<div className="emphasis-section-content">
 				<span className="emphasis-section-inner pb-3 block">
 					<h2 className="emphasis-section-heading max-w-6xl mx-auto font-heading font-bold lg:font-semibold tracking-tight leading-14.25 lg:leading-19.25 text-5xl lg:text-6xl text-gray-950 dark:text-gray-50 mb-7 lg:mb-4">{heading}</h2>
