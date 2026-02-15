3a890cf921ce30ded5f758fa85d51014a281e347
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-02-15 11:52:55 -0700

    Replace resume pdf

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,5 @@
 import { redirect, RedirectType } from 'next/navigation'
 
 export default function ResumePage() {
-	redirect('/assets/files/Resume_with_Cover_Letter_2026-01.pdf', RedirectType.push)
+	redirect('/assets/files/Resume_with_Cover_Letter_2026-02.pdf', RedirectType.push)
 }
