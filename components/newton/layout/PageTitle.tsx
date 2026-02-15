fa8c38e0eb04ca8fc27e8f01ea0a5afa76ef9015
Alden Gillespy <61519345+brazenest@users.noreply.github.com>
2026-02-15 11:52:09 -0700

    Modify about page

--- components/newton/layout/PageTitle.tsx
+++ components/newton/layout/PageTitle.tsx
@@ -15,9 +15,9 @@ export const PageTitle = ({ align = 'center', title, subtitle, subtitleClassName
 	}
 	return (
 		<span className={cn("page-title-wrap pb-1.25 block", className)}>
-			<h1 className={cn('page-title mb-3.75 md:mb-5 max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-5xl text-4xl md:text-5xl leading-10.25 md:leading-13.5 font-heading font-medium text-shadow-md', alignClasses[align].title)}>{title}</h1>
+			<h1 className={cn('page-title mb-3.75 md:mb-5 max-w-md sm:max-w-lg md:max-w-2xl lg:max-w-2xl text-4xl md:text-5xl leading-10.25 md:leading-13.5 font-heading font-medium text-shadow-md', alignClasses[align].title)}>{title}</h1>
 			{subtitle && (
-				<p className={cn('page-subtitle text-lg md:text-xl max-w-md sm:max-w-lg md:max-w-2xl text-gray-500 dark:text-gray-500 leading-6.25 md:leading-7', alignClasses[align].subtitle, subtitleClassName)}>{subtitle}</p>
+				<p className={cn('page-subtitle text-lg md:text-xl max-w-md sm:max-w-lg md:max-w-xl text-gray-500 dark:text-gray-500 leading-6.25 md:leading-7', alignClasses[align].subtitle, subtitleClassName)}>{subtitle}</p>
 			)}
 		</span>
 	)
